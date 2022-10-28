8c19a398a228414243838046818a536c2b101dcb
abdulla191332 <116917795+abdulla191332@users.noreply.github.com>
2022-10-28 22:04:07 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.main;
+
+/**
+ *
+ * @author abdulla
+p */
+public class  Main{
+ public static void main (String[]args){
+    //       0  1 2 3 4 5 6 7 8
+ //int [] y ={1,2,3,4,6,7,8};
+ //System.out.print(y[2]);
+ int [] [] y={{1,2,4,6}, {3,4,8,6},{5,10,-9,11}};
+  System.out.println(y[0][2]);
+  //y[0][3]=70;
+  //System.out.println(y[0][3]);
+System.out.println(y[2][3]);
+//System.out.println(y[0][6]);
+System.out.println(y[2][3]+3);
+ }
+}
+ 
+
+
+
+ 
+
+
+
+ 
+
+ 
+ 
+ 
+
+    
+    
+    
+   
+   
+        
+    
+    
+ 
+
+ 
+
+    
+    
+
+        
+        
+        
+        
+        
+    
+   
+    
+    
+
+   
+
